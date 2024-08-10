4613e02dabb9af0e817a897ebb00ad4d5bc55ed1
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-08-10 15:45:18 -0400

    Adds functionality

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -32,7 +32,7 @@ const resolvers = {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { $push: { savedBooks: { bookData } } },
+          { $push: { savedBooks: bookData  } },
           { new: true }
         );
         return updatedUser;
