698d9b344daa7bb2eade142c3b539e700493a96e
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-08-12 14:48:54 -0400

    Fixes server issue

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,20 +27,19 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === "production") {
+    console.log("production");
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-    })
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
   }
-  
 
   db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`graphql http://localhost:${PORT}/graphql`);
-    }
-    );
+      console.log(`graphql http://localhost:${PORT}/graphql`);
+    });
   });
 };
 
